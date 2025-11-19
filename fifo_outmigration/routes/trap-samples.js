b3e884ab232b68fd1aeb29f17f8650ac8824311a
Danielle <danielle@kendallspace.net>
2025-11-18 20:23:58 -0800

    fix(issue-1): update db connection, fix routes/controllers

--- fifo_outmigration/routes/trap-samples.js
+++ fifo_outmigration/routes/trap-samples.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const trapSamplesController = require('../controllers/trap-samples.js');
+const { ensureAuthenticated } = require('../middleware/auth');
 
-router.post('/trap-sample', trapSamplesController.submitForm);
+// protect all routes
+router.use(ensureAuthenticated);
+
+// api endpoints
+router.post('/', trapSamplesController.create);
+router.get('/', trapSamplesController.getAll);
+router.get('/:id', trapSamplesController.getById);
+router.put('/:id', trapSamplesController.update);
+router.delete('/:id', trapSamplesController.delete);
 
 module.exports = router;
